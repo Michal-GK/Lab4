55321315b7f100f531eb465004aca3124709a374
michal <michal@op.pl>
2022-05-11 19:22:00 +0200

    cz2Zadania

--- skrypt.sh
+++ skrypt.sh
@@ -5,9 +5,11 @@ function Help()
    echo "skrypt.sh"
    echo
    echo "Dostępne opcje:"
-   echo "--help	Wyświetla listę poleceń."
-   echo "--date	Wyświetla dzisiejszą datę."
-   echo "--logs [x]        Tworzy x plików log. (Domyślnie 100)"
+   echo "--help | -h	Wyświetla listę poleceń."
+   echo "--date | -d	Wyświetla dzisiejszą datę."
+   echo "--logs | -l [x]	Tworzy x plików log. (Domyślnie 100)"
+   echo "--error | -e [x]	Tworzy x plików error. (Domyślnie 100)"
+   echo "--init | -i	Klonuje repozytorium do folderu."
    echo
 }
 
@@ -18,10 +20,31 @@ function CreateLogs()
    fi
    for ((i = 1; i <= $LOGS; i++)); do
       echo "Tworzę log nr "$i
-      echo "log"$i".txt" >> log$i.txt
-      echo "$0" >> log$i.txt
-      date >> log$i.txt
+      mkdir log$i
+      echo "log"$i".txt" >> log$i/log$i.txt
+      echo "$0" >> log$i/log$i.txt
+      date >> log$i/log$i.txt
    done
+}
+
+function CreateErrors()
+{
+   if [[ $ERRORS == "" ]]; then
+      ERRORS=100
+   fi
+   for ((i = 1; i <= $ERRORS; i++)); do
+      echo "Tworzę error nr "$i
+      mkdir error$i
+      echo "error"$i".txt" >> error$i/error$i.txt
+      echo "$0" >> error$i/error$i.txt
+      date >> error$i/error$i.txt
+   done
+}
+
+function Init()
+{
+   git clone git@github.com:Michal-GK/Lab4.git .
+   export PATH=$PATH:.
 }
 
 if [ $# -eq 0 ]; then
@@ -30,19 +53,28 @@ if [ $# -eq 0 ]; then
 fi
 
 LOGS=
+ERRORS=
 
 while [ "$1" != "" ]; do
     case $1 in
-    --help)
+    -h | --help)
         Help
         ;;
-    --date)
+    -d | --date)
         date
         ;;
-    --logs)
+    -l | --logs)
         shift 
         LOGS=$1
         CreateLogs
+        ;;
+    -e | --error)
+        shift 
+        ERRORS=$1
+        CreateErrors
+        ;;
+    -i | --init)
+        Init
         ;;
     *)
         echo "Błąd, niepoprawne parametry"
